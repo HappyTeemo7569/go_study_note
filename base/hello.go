b67cb33ba558365d5c02aee155d1d67431cec4ea
HappyTeemo7569 <1726214027@qq.com>
2021-10-09 17:30:48 +0800

    select

--- base/hello.go
+++ base/hello.go
@@ -1,4 +1,4 @@
-package base
+package main
 
 //1) go语言以包作为管理单位
 //2) 每个文件必须先声明包
@@ -8,7 +8,7 @@ import "fmt"
 
 //fmt 包实现了格式化 IO（输入/输出）
 
-func HelloWord() { //左括号必须和函数名同行
+func main() { //左括号必须和函数名同行
 
 	//打印
 	//"hello go"打印到屏幕， Println()会自动换行

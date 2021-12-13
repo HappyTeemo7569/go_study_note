08f44888280dd0ed05b257ac8a2bce70cb1a9ff1
HappyTeemo7569 <1726214027@qq.com>
2021-12-13 10:35:39 +0800

    feat: go

--- goroutine/go_1.go
+++ goroutine/go_1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
@@ -22,7 +23,13 @@ func main() {
 	//死循环
 	for {
 		time.Sleep(time.Second) //延时1s
+
+		runtime.Gosched() //让别人先执行，需要同时要时间片的时候才会有效，对方如果已经停了就还是自己执行。
+
 		fmt.Println("主进程 index", index)
 		index++
 	}
+
+	// 主goroutine退出后，其它的工作goroutine也会自动退出
+	// 多个 goroutine 的执行顺序不定
 }

b7a14e1533da8c124e75efb72b9e55dd6c84ebe7
HappyTeemo7569 <1726214027@qq.com>
2022-01-24 17:30:36 +0800

    测试
    测试

---
+++ algorithm/my_stack/link_stack_test.go
@@ -0,0 +1,12 @@
+package my_stack
+
+import (
+	"testing"
+)
+
+func TestLinkStack(t *testing.T) {
+
+	my_list := new(LinkStack)
+	my_list.Test()
+
+}
---
+++ algorithm/my_stack/sq_stack_test.go
@@ -0,0 +1,10 @@
+package my_stack
+
+import "testing"
+
+func TestSqStack(t *testing.T) {
+
+	my_list := new(SqStack)
+	my_list.Test()
+
+}

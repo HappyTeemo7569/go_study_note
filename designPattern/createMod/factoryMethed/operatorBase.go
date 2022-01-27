53caa2cbb626e87b5b4035b714fd30278d9c49c3
HappyTeemo7569 <1726214027@qq.com>
2022-01-27 11:15:07 +0800

    feat : 工厂模式

---
+++ designPattern/createMod/factoryMethed/operatorBase.go
@@ -0,0 +1,16 @@
+package factoryMethod
+
+//OperatorBase 是Operator 接口实现的基类，封装公用方法
+type OperatorBase struct {
+	a, b int
+}
+
+//SetA 设置 A
+func (o *OperatorBase) SetA(a int) {
+	o.a = a
+}
+
+//SetB 设置 B
+func (o *OperatorBase) SetB(b int) {
+	o.b = b
+}

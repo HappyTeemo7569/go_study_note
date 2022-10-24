10293ca550bdbfa1b9f85dbdf4b6e38d4c809ce7
HappyTeemo7569 <1726214027@qq.com>
2022-10-24 10:48:58 +0800

    feat : 容器，双向链表

---
+++ base/dataType/list.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	l := list.New()
+
+	l.PushBack("test1")
+	l.PushFront(11)
+	showList(l)
+
+	element := l.PushBack("test2")
+	showList(l)
+	l.InsertAfter("test_after_2", element)
+	showList(l)
+	l.InsertBefore("test_before_2", element)
+	showList(l)
+	l.Remove(element)
+	showList(l)
+
+}
+
+func showList(l *list.List) {
+	fmt.Println("-------------------")
+	e_tmp := l.Front()
+	for i := 0; i < l.Len(); i++ {
+		fmt.Println(e_tmp.Value)
+		e_tmp = e_tmp.Next()
+	}
+	fmt.Println("-------------------")
+}

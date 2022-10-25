653e006d74377b7b674fc1b1cee66245d5f9a8d8
HappyTeemo7569 <1726214027@qq.com>
2022-10-25 17:16:19 +0800

    feat：整理

--- main.go
+++ main.go
@@ -1,38 +1,10 @@
 package main //包名
 
 import (
-	"my_go/goroutine"
 	_ "my_go/myLib"
 	"my_go/web"
 )
 
 func webTest() {
 	web.RunServer()
-}
-
-func goroutineTest() {
-	//goroutine.RunM()
-	//goroutine.RunC()
-	//goroutine.RunL()
-	goroutine.RunCond()
-}
-
-func main() {
-
-	//go func() {
-	//	myScoket.RunTCP()
-	//}()
-	//go func() {
-	//	myScoket.RunUDP()
-	//}()
-	//
-	//ch := make(chan int)
-	//ch <- 1
-
-	//test := my_stack.LinkStack{}
-	//test.Test()
-	//fmt.Println("OK ")
-
-	goroutineTest()
-
 }
